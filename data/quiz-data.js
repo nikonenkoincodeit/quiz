9c1d7d74655d7040953171b8a371b5283bbcd4d8
nikonenkoincodeit <75611895+nikonenkoincodeit@users.noreply.github.com>
2024-03-03 22:00:06 +0200

    fix

--- data/quiz-data.js
+++ data/quiz-data.js
@@ -11,52 +11,56 @@ export const quizMirrors = [
       {
         id: 1,
         text: "Просто зеркало- без подсветки и граней",
-        img: "../img/mirrors/synxpdsqel7l27vvx3hj.webp",
+        img: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/synxpdsqel7l27vvx3hj.webp",
       },
       {
         id: 2,
         text: "Зеркальная плитка",
-        img: "../img/mirrors/iuta0ofcfga5wapjwgoz.webp",
+        img: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/iuta0ofcfga5wapjwgoz.webp",
       },
       {
         id: 3,
         text: "Зеркальное панно",
-        img: "../img/mirrors/p1pkloi0rpylwcyzhd9u.webp",
+        img: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/p1pkloi0rpylwcyzhd9u.webp",
       },
       {
         id: 4,
         text: "С фацетом",
-        img: "../img/mirrors/ahuxlvu57kxlbxbcrno5.webp",
+        img: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/ahuxlvu57kxlbxbcrno5.webp",
       },
       {
         id: 5,
         text: "С алмазной гравировкой",
-        img: "../img/mirrors/qmm6n9uzxrnx7dhtqcsq.webp",
+        img: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/qmm6n9uzxrnx7dhtqcsq.webp",
       },
       {
         id: 6,
         text: "с витражем CRI",
-        img: "../img/mirrors/ki5gxrbjnompgm1kh2cu.webp",
+        img: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/ki5gxrbjnompgm1kh2cu.webp",
       },
       {
         id: 7,
         text: "С пескоструем",
-        img: "../img/mirrors/bo0ff6ydxxn3bptcwlzf.webp",
+        img: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/bo0ff6ydxxn3bptcwlzf.webp",
       },
       {
         id: 8,
         text: "С подсветкой",
-        img: "../img/mirrors/gvim6u3ht9imxjncmnvh.webp",
+        img: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/gvim6u3ht9imxjncmnvh.webp",
       },
       {
         id: 9,
         text: "Зеркало для шкафа-купе",
-        img: "../img/mirrors/mxlrxr8ylshyb2cryjcc.webp",
+        img: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/mxlrxr8ylshyb2cryjcc.webp",
       },
-      { id: 10, text: "Не могу определиться", img: "../img/mirrors/grey.jpg" },
+      {
+        id: 10,
+        text: "Не могу определиться",
+        img: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/grey.jpg",
+      },
     ],
     assistant: {
-      avatar: "../img/mirrors/daria.webp",
+      avatar: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/daria.webp",
       name: "Дарья",
       specialty: "специалист по зеркалам",
       text: "Смотри подсказку→→→ Если не понимаете в чем разница, но просто выберите последний пункт 'Не могу определиться' и менеджер поможет Вам и разъяснит все, что нужно.",
@@ -87,7 +91,7 @@ export const quizMirrors = [
       },
     ],
     assistant: {
-      avatar: "../img/mirrors/daria.webp",
+      avatar: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/daria.webp",
       name: "Дарья",
       specialty: "специалист по зеркалам",
       text: "Смотри подсказку→→→ Если да, то предложим загрузить Ваш файл или фото в следующем шаге. Либо можете отправить нам фотографию чертежей в Viber.",
@@ -121,7 +125,7 @@ export const quizMirrors = [
     title:
       "Напишите тут комментарий или размеры, уточнения, пожелания, ссылку на картинку",
     assistant: {
-      avatar: "../img/mirrors/daria.webp",
+      avatar: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/daria.webp",
       name: "Дарья",
       specialty: "специалист по зеркалам",
       text: "Смотри подсказку→→→ Впишите дополнительную информацию. Например, что еще нам стоит учесть или укажите, что для Вас важнее всего в случае заказа (скорость, качество, точность, под ключ, доставка, установка, цена, сложные изделия)",
@@ -137,7 +141,7 @@ export const quizMirrors = [
     title:
       "Напишите тут комментарий или размеры, уточнения, пожелания, ссылку на картинку",
     assistant: {
-      avatar: "../img/mirrors/daria.webp",
+      avatar: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/daria.webp",
       name: "Дарья",
       specialty: "специалист по зеркалам",
       text: "Смотри подсказку→→→ Впишите дополнительную информацию. Например, что еще нам стоит учесть или укажите, что для Вас важнее всего в случае заказа (скорость, качество, точность, под ключ, доставка, установка, цена, сложные изделия)",
@@ -153,7 +157,7 @@ export const quizMirrors = [
     title:
       "Напишите тут комментарий или размеры, уточнения, пожелания, ссылку на картинку",
     assistant: {
-      avatar: "../img/mirrors/daria.webp",
+      avatar: "https://nikonenkoincodeit.github.io/quiz/img/mirrors/daria.webp",
       name: "Дарья",
       specialty: "специалист по зеркалам",
       text: "Смотри подсказку→→→ Впишите дополнительную информацию. Например, что еще нам стоит учесть или укажите, что для Вас важнее всего в случае заказа (скорость, качество, точность, под ключ, доставка, установка, цена, сложные изделия)",
